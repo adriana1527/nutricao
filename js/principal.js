35848ffb8326e0649b8e66a43df7f496fe1b0f89
adriana <adriana.siqueira.reis@escola.pr.gov.br>
2025-10-24 11:11:40 -0300

    55

--- js/principal.js
+++ js/principal.js
@@ -1,7 +1,60 @@
-   console.log(document.querySelector("h1"));
-   var titulo = document.querySelector("h1");
-   console.log(titulo);
-   console.log(titulo.textContent);
-   titulo.textContent = ("vladismara Nutricionista");
+var titulo = document.querySelector("h1");
+console.log(titulo);
+console.log(titulo.textContent);
+titulo.textContent = ("Aparecida Nutricionista");
+
+
+21/10
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
     
-   
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+        // paciente.style.backgroundColor = "red";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
+    }
+
+  if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+}
+
+titulo.addEventListener("click", mostraMensagem);
+function mostraMensagem(){
+    alert("Olá eu fui clicado.")
+}
+
+titulo.addEventListener("click", function(){
+    alert("Ola eu fui clicado numa funcao anonima");
+});
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+    // alert("Oi eu sou o botao e fui clicado");
+    var form = document.querySelector("#form-adiciona");
